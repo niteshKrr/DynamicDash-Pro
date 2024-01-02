16197207ec8118078c65a0d14ebdfcea942ce5d7
niteshKrr <nkumar35101@gmail.com>
2024-01-02 12:25:48 +0530

    home page edited

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div>Hello...</div>
+      <Link href="/dashboard">
+      <div className="h-screen flex items-center justify-center text-2xl">Go To Dashboard...</div>
+      </Link>
     </>
   );
 }
